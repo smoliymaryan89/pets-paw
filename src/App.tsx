cec9319707b42cd0b16e13f267abec7432543019
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2024-02-23 14:39:24 +0200

    feat: create app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import { Route, Routes } from "react-router-dom";
+
+import Layout from "@layouts/Layout";
+import HomePage from "@pages/HomePage";
+import BreedsPage from "@pages/BreedsPage";
+import BreedDetailsPage from "@pages/BreedDetailsPage";
+import SearchPage from "@pages/SearchPage";
+import DisLikesPage from "@pages/DisLikesPage";
+import FavouritesPage from "@pages/FavouritesPage";
+import GalleryPage from "@pages/GalleryPage";
+import LikesPage from "@pages/LikesPage";
+import VotingPage from "@pages/VotingPage";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="breeds" element={<BreedsPage />} />
+        <Route path="breeds/:breedId" element={<BreedDetailsPage />} />
+        <Route path="search" element={<SearchPage />} />
+        <Route path="dislikes" element={<DisLikesPage />} />
+        <Route path="likes" element={<LikesPage />} />
+        <Route path="favourites" element={<FavouritesPage />} />
+        <Route path="gallery" element={<GalleryPage />} />
+        <Route path="voting" element={<VotingPage />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
