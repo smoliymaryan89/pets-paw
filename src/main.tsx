063573b8502f57604235d11b31f2f071845d009f
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2024-02-23 16:17:54 +0200

    feat: add TanstackProvider to main

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import App from "./App.tsx";
 import "./index.css";
 
 import { BrowserRouter } from "react-router-dom";
+import TanstackProvider from "@utils/providers/TanstackProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <TanstackProvider>
+        <App />
+      </TanstackProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
