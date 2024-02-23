17be57f3070b6766c1b5e212adffaafef079c23a
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2024-02-23 16:54:22 +0200

    feat: create back btn

---
+++ src/components/ui/BackBtn.tsx
@@ -0,0 +1,46 @@
+import { useLocation, useNavigate } from "react-router-dom";
+
+import clsx from "clsx";
+import Button from "./Button";
+
+interface BackBtnProps {
+  text: string;
+  additionalText?: string;
+  className?: string;
+}
+
+const BackBtn = ({ text, additionalText, className }: BackBtnProps) => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  return (
+    <div
+      className={clsx(
+        "flex flex-wrap items-center gap-[10px]",
+        pathname === "/breeds" || pathname === "/gallery" ? "mb-0" : "mb-[20px]"
+      )}
+    >
+      <Button
+        icon="icon-back"
+        btnStyles="hover:bg-pink"
+        iconStyles="group-hover:fill-white"
+        onClick={() => navigate(-1)}
+      />
+      <p
+        className={clsx(
+          "text-white bg-pink rounded-10 py-[5px] px-[30px] text-20 font-medium leading-normal tracking-[2px]",
+          className
+        )}
+      >
+        {text}
+      </p>
+      {additionalText && (
+        <span className="text-white bg-pink rounded-10 py-[5px] px-[20px] text-20 font-medium leading-normal tracking-[2px] md:px-[30px]">
+          {additionalText}
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default BackBtn;
