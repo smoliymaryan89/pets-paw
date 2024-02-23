b2b2b4254a33f58723ae0104b2cec74e0f83246d
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2024-02-23 23:23:49 +0200

    feat: create breeds details page

---
+++ src/pages/BreedDetailsPage.tsx
@@ -0,0 +1,39 @@
+import { useParams } from "react-router-dom";
+import { useBreedByParams } from "@services/breed/query";
+
+import Slider from "@components/Slider";
+import Loader from "@components/ui/Loader";
+import BackBtn from "@components/ui/BackBtn";
+import BreedDescription from "@components/BreedDescription";
+
+const BreedDetailsPage = () => {
+  const { breedId } = useParams();
+
+  const { data: breed, isFetching } = useBreedByParams({
+    breed_ids: breedId ?? "",
+    limit: String(5),
+  });
+
+  return (
+    <section className="section">
+      <BackBtn
+        text="BREEDS"
+        additionalText={breedId}
+        className="!bg-light-pink !text-pink"
+      />
+
+      {isFetching ? (
+        <Loader className="h-[520px] md:h-[606px]" />
+      ) : (
+        breed && (
+          <>
+            <Slider data={breed} />
+            <BreedDescription data={breed} />
+          </>
+        )
+      )}
+    </section>
+  );
+};
+
+export default BreedDetailsPage;
