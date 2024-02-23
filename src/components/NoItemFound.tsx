e164a781fa8a68c6f527e64946f1cc80a32a1c06
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2024-02-23 20:22:39 +0200

    feat: create no item found

---
+++ src/components/NoItemFound.tsx
@@ -0,0 +1,9 @@
+const NoItemFound = () => {
+  return (
+    <p className="py-[18px] px-[20px] leading-normal bg-dark-white rounded-10">
+      No item found
+    </p>
+  );
+};
+
+export default NoItemFound;
