413b2f42f34f285d45ef8f62b713640d7ad50843
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2024-02-25 16:30:59 +0200

    feat: add toaster and add user id

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
+import { useEffect } from "react";
+import { nanoid } from "nanoid";
 import { Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 import Layout from "@layouts/Layout";
 import HomePage from "@pages/HomePage";
@@ -12,20 +15,30 @@ import LikesPage from "@pages/LikesPage";
 import VotingPage from "@pages/VotingPage";
 
 const App = () => {
+  useEffect(() => {
+    if (!localStorage.getItem("user-id")) {
+      localStorage.setItem("user-id", nanoid());
+    }
+  }, []);
+
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="breeds" element={<BreedsPage />} />
-        <Route path="breeds/:breedId" element={<BreedDetailsPage />} />
-        <Route path="search" element={<SearchPage />} />
-        <Route path="dislikes" element={<DisLikesPage />} />
-        <Route path="likes" element={<LikesPage />} />
-        <Route path="favourites" element={<FavouritesPage />} />
-        <Route path="gallery" element={<GalleryPage />} />
-        <Route path="voting" element={<VotingPage />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="breeds" element={<BreedsPage />} />
+          <Route path="breeds/:breedId" element={<BreedDetailsPage />} />
+          <Route path="search" element={<SearchPage />} />
+          <Route path="dislikes" element={<DisLikesPage />} />
+          <Route path="likes" element={<LikesPage />} />
+          <Route path="favourites" element={<FavouritesPage />} />
+          <Route path="gallery" element={<GalleryPage />} />
+          <Route path="voting" element={<VotingPage />} />
+        </Route>
+      </Routes>
+
+      <Toaster position="bottom-center" />
+    </>
   );
 };
 
