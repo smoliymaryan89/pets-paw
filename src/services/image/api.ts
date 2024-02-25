abad7a1e7f54a07a44a562c4a66b9eafd2a8c670
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2024-02-25 16:47:42 +0200

    feat: add res types

--- src/services/image/api.ts
+++ src/services/image/api.ts
@@ -1,7 +1,22 @@
+import { AxiosResponse } from "axios";
+
 import instance from "@services/instance";
 
-export const uploadImage = async (data: FormData) => {
-  return await instance.post("/images/upload", data, {
+interface UploadImageResponse {
+  approved: number;
+  height: number;
+  id: string;
+  original_filename: string;
+  pending: number;
+  sub_id: string;
+  url: string;
+  width: number;
+}
+
+export const uploadImage = async (
+  data: FormData
+): Promise<AxiosResponse<UploadImageResponse>> => {
+  return await instance.post<UploadImageResponse>("/images/upload", data, {
     headers: { "Content-Type": "multipart/form-data" },
   });
 };
