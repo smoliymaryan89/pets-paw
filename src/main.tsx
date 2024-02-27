6105a89e8e6c17b8e9c55f1509cbebcce42d123a
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2024-02-27 14:41:13 +0200

    feat: add FavouriteProvider to app

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import App from "./App.tsx";
 import "./index.css";
 
 import { BrowserRouter } from "react-router-dom";
+
 import TanstackProvider from "@utils/providers/TanstackProvider.tsx";
+import FavouriteProvider from "@context/FavouriteContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <TanstackProvider>
-        <App />
+        <FavouriteProvider>
+          <App />
+        </FavouriteProvider>
       </TanstackProvider>
     </BrowserRouter>
   </React.StrictMode>
