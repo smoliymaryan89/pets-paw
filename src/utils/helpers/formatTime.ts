1ead46eb42fa804d0e6720bfd2777492bd0ff04e
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2024-02-27 14:37:43 +0200

    feat: create format time func

---
+++ src/utils/helpers/formatTime.ts
@@ -0,0 +1,9 @@
+const formatTime = (timeString: string): string => {
+  const date = new Date(timeString);
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+
+  return `${hours}:${minutes}`;
+};
+
+export default formatTime;
