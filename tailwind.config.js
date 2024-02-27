34028dc1dbd2604d8a8f92df25f424e75aeca92b
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2024-02-27 21:37:24 +0200

    style: add new style variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
         dark: "#1D1D1D",
+        "light-dark": "#282828",
         grey: "#8C8C8C",
         "grey-60": "rgba(255, 255, 255, 0.60)",
+        "grey-0.05": "rgba(255, 255, 255, 0.05)",
+        "grey-10": "rgba(255, 255, 255, 0.10)",
         pink: "#FF868E",
         "light-pink": "#FBE0DC",
         "pink-60": "rgba(255, 134, 142, 0.60)",
         "pink-30": "rgba(255, 134, 142, 0.30)",
+        "pink-20": "rgba(255, 134, 142, 0.20)",
         "dark-white": "#F8F8F7",
         green: "#97EAB9",
         "green-30": "rgba(151, 234, 185, 0.30)",
@@ -38,7 +43,10 @@ export default {
         3: "3px",
         5: "5px",
         10: "10px",
+        14: "14px",
         20: "20px",
+        24: "24px",
+        50: "50px",
       },
       fontFamily: {
         jost: ["Jost", "sans-serif"],
