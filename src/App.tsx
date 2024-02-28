3e57cf081027bdc55eef206ece3e158190bac5df
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2024-02-28 12:00:11 +0200

    feat: add lazy routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import { useEffect } from "react";
+import { lazy, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { nanoid } from "nanoid";
 
 import Layout from "@layouts/Layout";
-import HomePage from "@pages/HomePage";
-import BreedsPage from "@pages/BreedsPage";
-import BreedDetailsPage from "@pages/BreedDetailsPage";
-import SearchPage from "@pages/SearchPage";
-import DisLikesPage from "@pages/DisLikesPage";
-import FavouritesPage from "@pages/FavouritesPage";
-import GalleryPage from "@pages/GalleryPage";
-import LikesPage from "@pages/LikesPage";
-import VotingPage from "@pages/VotingPage";
+import SuspenseWithLoader from "@components/SuspenseWithLoader";
+
+const HomePage = lazy(() => import("@pages/HomePage"));
+const BreedsPage = lazy(() => import("@pages/BreedsPage"));
+const BreedDetailsPage = lazy(() => import("@pages/BreedDetailsPage"));
+const SearchPage = lazy(() => import("@pages/SearchPage"));
+const DisLikesPage = lazy(() => import("@pages/DisLikesPage"));
+const FavouritesPage = lazy(() => import("@pages/FavouritesPage"));
+const GalleryPage = lazy(() => import("@pages/GalleryPage"));
+const LikesPage = lazy(() => import("@pages/LikesPage"));
+const VotingPage = lazy(() => import("@pages/VotingPage"));
 
 const App = () => {
   useEffect(() => {
@@ -29,15 +31,78 @@ const App = () => {
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="breeds" element={<BreedsPage />} />
-          <Route path="breeds/:breedId" element={<BreedDetailsPage />} />
-          <Route path="search" element={<SearchPage />} />
-          <Route path="dislikes" element={<DisLikesPage />} />
-          <Route path="likes" element={<LikesPage />} />
-          <Route path="favourites" element={<FavouritesPage />} />
-          <Route path="gallery" element={<GalleryPage />} />
-          <Route path="voting" element={<VotingPage />} />
+          <Route
+            index
+            element={
+              <SuspenseWithLoader>
+                <HomePage />
+              </SuspenseWithLoader>
+            }
+          />
+          <Route
+            path="breeds"
+            element={
+              <SuspenseWithLoader>
+                <BreedsPage />
+              </SuspenseWithLoader>
+            }
+          />
+          <Route
+            path="breeds/:breedId"
+            element={
+              <SuspenseWithLoader>
+                <BreedDetailsPage />
+              </SuspenseWithLoader>
+            }
+          />
+          <Route
+            path="search"
+            element={
+              <SuspenseWithLoader>
+                <SearchPage />
+              </SuspenseWithLoader>
+            }
+          />
+          <Route
+            path="dislikes"
+            element={
+              <SuspenseWithLoader>
+                <DisLikesPage />
+              </SuspenseWithLoader>
+            }
+          />
+          <Route
+            path="likes"
+            element={
+              <SuspenseWithLoader>
+                <LikesPage />
+              </SuspenseWithLoader>
+            }
+          />
+          <Route
+            path="favourites"
+            element={
+              <SuspenseWithLoader>
+                <FavouritesPage />
+              </SuspenseWithLoader>
+            }
+          />
+          <Route
+            path="gallery"
+            element={
+              <SuspenseWithLoader>
+                <GalleryPage />
+              </SuspenseWithLoader>
+            }
+          />
+          <Route
+            path="voting"
+            element={
+              <SuspenseWithLoader>
+                <VotingPage />
+              </SuspenseWithLoader>
+            }
+          />
         </Route>
       </Routes>
 
